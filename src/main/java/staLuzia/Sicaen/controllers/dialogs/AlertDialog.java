e8cf153d85c3da38c1a3bbd9514e0aaf722996fc
Henrique Dreyer <henriquedreyer@gmail.com>
2016-05-08 19:24:36 -0300

    implementacao do warningDialog e exceptionDialog

--- src/main/java/staLuzia/Sicaen/controllers/dialogs/AlertDialog.java
+++ src/main/java/staLuzia/Sicaen/controllers/dialogs/AlertDialog.java
@@ -1,6 +1,10 @@
 package staLuzia.Sicaen.controllers.dialogs;
 
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 import staLuzia.Sicaen.models.dialogMessages.DialogMessage;
 
 /**
@@ -14,7 +18,42 @@ public class AlertDialog {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle(title.toString());
         alert.setHeaderText(message.toString());
-        //alert.setContentText(content);
+
+        alert.showAndWait();
+    }
+
+    public void showWarningDialog(DialogMessage title, DialogMessage header,DialogMessage content){
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle(title.toString());
+        alert.setHeaderText(header.toString());
+        alert.setContentText(content.toString());
+
+        alert.showAndWait();
+    }
+
+    public void showExceptionDialog(DialogMessage title, DialogMessage message, Exception exception){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title.toString());
+        alert.setHeaderText(message.toString());
+
+        Label label = new Label("O erro gerou o segunte relatório:");
+
+        TextArea textArea = new TextArea(exception.toString());
+        textArea.setEditable(false);
+        textArea.setWrapText(true);
+
+        textArea.setMaxWidth(Double.MAX_VALUE);
+        textArea.setMaxHeight(Double.MAX_VALUE);
+        GridPane.setVgrow(textArea, Priority.ALWAYS);
+        GridPane.setHgrow(textArea, Priority.ALWAYS);
+
+        GridPane expContent = new GridPane();
+        expContent.setMaxWidth(Double.MAX_VALUE);
+        expContent.add(label, 0, 0);
+        expContent.add(textArea, 0, 1);
+
+        // Set expandable Exception into the dialog pane.
+        alert.getDialogPane().setExpandableContent(expContent);
 
         alert.showAndWait();
     }
