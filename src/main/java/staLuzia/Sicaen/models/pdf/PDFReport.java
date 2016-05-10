9c95f554143dca4e5e52050fb3e7d94a9474a621
Henrique Dreyer <henriquedreyer@gmail.com>
2016-05-10 00:50:14 -0300

    implementacao do metodo generateEncontristaReport

--- src/main/java/staLuzia/Sicaen/models/pdf/PDFReport.java
+++ src/main/java/staLuzia/Sicaen/models/pdf/PDFReport.java
@@ -24,13 +24,58 @@ public class PDFReport {
     public void generateEncontristaReport(List<Encontrista> list) throws DocumentException {
         try {
             for(Encontrista enc : list) {
-                PdfReader pdfTemplate = new PdfReader("template/pdf/pdfModel.pdf");
+                PdfReader pdfTemplate = new PdfReader(PDFModelFields.PDF_FILE_MODEL_PATH.toString());
                 FileOutputStream fileOutputStream = new FileOutputStream("relatorio/" +enc.getNome()+".pdf");
                 ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
                 PdfStamper pdfStamper = new PdfStamper(pdfTemplate, fileOutputStream);
                 pdfStamper.setFormFlattening(true);
 
-                pdfStamper.getAcroFields().setField("nome",enc.getNome().toUpperCase());
+                //PUTA QUE PARIU! É MUITO CAMPO PARA PREENCHER
+                //CAMPOS PARA PREENCHER NO PDF
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_NOME.toString(),
+                        enc.getNome().toUpperCase());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_APELIDO.toString(),
+                        enc.getApelido());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_RELACIONAMENTO.toString(),
+                        enc.getRelacionamento());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_DATANASCIMENTO.toString(),
+                        enc.getDataNascimento().toString());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_CELULAR.toString(),
+                        enc.getCelular());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_WHATSAPP.toString(),
+                        enc.getWhatsapp());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_RESPONSAVEL1.toString(),
+                        enc.getNomeResponsavel1());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_RESPONSAVEL1_TELEFONE.toString(),
+                        enc.getTelefoneResponsavel1());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_RESPONSAVEL2.toString(),
+                        enc.getNomeResponsavel2());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_RESPONSAVEL2_TELEFONE.toString(),
+                        enc.getTelefoneResponsavel2());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_EMAIL.toString(),
+                        enc.getEmail());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_TAMANHOCAMISA.toString(),
+                        enc.getTamanhoCamisa().toUpperCase());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_PROBLEMASAUDE.toString(),
+                        enc.getProblemaSaude());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_PROBLEMAALIMENTAR.toString(),
+                        enc.getProblemaAlimentar());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_ENDERECO.toString(),
+                        enc.getEndereco());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_COMPLEMENTO.toString(),
+                        enc.getComplemento());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_CEP.toString(),
+                        enc.getCep());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_BAIRRO.toString(),
+                        enc.getBairro());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_REFERENCIA.toString(),
+                        enc.getReferencia());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_RELIGIAO.toString(),
+                        enc.getReligiao());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_PAROQUIA.toString(),
+                        enc.getParoquia());
+                pdfStamper.getAcroFields().setField(PDFModelFields.ACROFIELD_SACRAMENTO.toString(),
+                        enc.getSacramento());
 
                 Image image = Image.getInstance(enc.getImagemPerfil());
                 image.scaleAbsolute(4.80f, 5.99f); //Tamanho da imagem no formulario
